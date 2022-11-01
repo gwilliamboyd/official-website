ae67896b375b93ea707291703c44c46465d51dff
gwilliamboyd <gwillboyd@outlook.com>
2022-10-31 20:59:17 -0500

    Mobile drop down menu works
    Mobile drop down menu is now functional, and all pages have been
    updated to show it.
    
    Next, the content for the other 3 pages will be finalized.

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-function displayMenu() {
-    console.log("Mobile menu clicked!");
-    const navBarLinks = document.getElementById("nav-bar-links");
-    if(navBarLinks.style.display === "block") {
-        navBarLinks.style.display === "none";
-    } else {
-        navBarLinks.style.display === "block";
-    }
-}
+const mobileMenu = document.getElementById("mobile-menu-icon");
+const navBarLinks = document.getElementById("nav-bar-links");
+navBarLinks.style.display = "none";
+
+let menuActive = false;
+
+mobileMenu.addEventListener("click", () => {
+    navBarLinks.style.display = navBarLinks.style.display == "none" ? "flex" : "none";
+    });
+
+// function toggleMenu() {
+//     console.log("Hello world!");    
+
+//     // if(navBarLinks.style.display = "none") {
+//     //     console.log("HIDDEN - needs to be displayed");
+//     //     navBarLinks.style.display = "flex";
+//     //     console.log("SHOWN - menu is now displayed!");
+//     //     menuActive = true;
+//     //     console.log(menuActive);
+//     // }  
+//     // else {
+//     //     console.log("ACTIVE - the menu is showing");
+//     //     navBarLinks.style.display = "none";
+//     //     console.log("CLOSED - the menu is now closed");
+//     //     menuActive = false;
+//     // } 
+// }
