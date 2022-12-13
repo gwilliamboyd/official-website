028eaf10a91eb45074d1379f340c36f11855ec0c
gwilliamboyd <gwillboyd@outlook.com>
2022-12-13 12:16:18 -0600

    Scroll to element works with offset
    Currently, the offset is determined with a hard pixel value
    The navbar height is returning null in my javascript file. I believe
    this is because I didn't actually set a 'height' attribute in my
    navbar's CSS, the height is only determined by the content + padding.
    
    The goal is to set the offset of the scroll to be equal to the height
    of the navbar, whether in mobile, tablet or desktop configuration.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Mobile menu - show and hide
 const mobileMenu = document.getElementById("mobile-menu-icon");
 const mobileMenuLinks = document.getElementById("mobile-menu-links");
 mobileMenuLinks.style.display = "none";
@@ -7,3 +8,51 @@ let menuActive = false;
 mobileMenu.addEventListener("click", () => {
     mobileMenuLinks.style.display = mobileMenuLinks.style.display == "none" ? "flex" : "none";
     });
+
+// Scroll down home page - offset by nav bar height
+
+/* function scrollWork() {
+    const navbar = document.getElementById('#nav-bar');
+    //const navbarHeight = navbar.style.height;
+    //console.log(navbarHeight);
+
+    const workBody = document.getElementById("body-work");    
+    workBody.scrollIntoView().window.scrollBy(0, -50);
+    //window.scrollBy(0, 50);
+    //window.scroll
+    console.log(window.screenY);
+  } */
+
+function scrollWork(){
+    const workBody = document.getElementById('body-work');
+    let headerOffset = 80;
+    let workBodyPosition = workBody.getBoundingClientRect().top;
+    let offsetPosition = workBodyPosition + window.pageYOffset - headerOffset;
+
+    window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+    });
+}
+function scrollAboutMe(){
+    const aboutMeBody = document.getElementById('body-about-me');
+    let headerOffset = 120;
+    let aboutMeBodyPosition = aboutMeBody.getBoundingClientRect().top;
+    let offsetPosition = aboutMeBodyPosition + window.pageYOffset - headerOffset;
+
+    window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+    });
+}
+function scrollContact(){
+    const contactBody = document.getElementById('body-contact');
+    let headerOffset = 30;
+    let contactBodyPosition = contactBody.getBoundingClientRect().top;
+    let offsetPosition = contactBodyPosition + window.pageYOffset - headerOffset;
+
+    window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+    });
+}
