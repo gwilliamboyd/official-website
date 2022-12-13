d437536f54ecc7e49a977429a9380d7e5b8f82d1
gwilliamboyd <gwillboyd@outlook.com>
2022-12-13 11:22:31 -0600

    QoL changes
    Changed some ID names to make them easier to read
    Changed font weight on nav bar

--- app.js
+++ app.js
@@ -7,23 +7,3 @@ let menuActive = false;
 mobileMenu.addEventListener("click", () => {
     mobileMenuLinks.style.display = mobileMenuLinks.style.display == "none" ? "flex" : "none";
     });
-
-// OLD METHOD - W3 Schools - didn't work
-
-// function toggleMenu() {
-//     console.log("Hello world!");    
-
-//     // if(navBarLinks.style.display = "none") {
-//     //     console.log("HIDDEN - needs to be displayed");
-//     //     navBarLinks.style.display = "flex";
-//     //     console.log("SHOWN - menu is now displayed!");
-//     //     menuActive = true;
-//     //     console.log(menuActive);
-//     // }  
-//     // else {
-//     //     console.log("ACTIVE - the menu is showing");
-//     //     navBarLinks.style.display = "none";
-//     //     console.log("CLOSED - the menu is now closed");
-//     //     menuActive = false;
-//     // } 
-// }
