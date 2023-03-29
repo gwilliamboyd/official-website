16e5bc1092fefa164e4771a01f1acf5a92fb2a08
gwilliamboyd <gwillboyd@outlook.com>
2023-03-29 12:11:29 -0500

    Mobile menu fixes
    Menu now scrolls with you down the page
    Mobile menu opacity increased from 75% to 90%
    Menu now closes when clicked outside, or when clicking a link

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-// Mobile menu - show and hide
-const mobileMenu = document.getElementById("mobile-menu-icon");
-const mobileMenuLinks = document.getElementById("mobile-menu-links");
-mobileMenuLinks.style.display = "none";
+// DOM references
+const docBody = document.querySelector('body')
+const mobileMenu = document.getElementById('mobile-menu-icon')
+const mobileMenuLinks = document.getElementById('mobile-menu-links')
+mobileMenuLinks.style.display = 'none'
 
-let menuActive = false;
+let menuActive = false
 
-mobileMenu.addEventListener("click", () => {
-    mobileMenuLinks.style.display = mobileMenuLinks.style.display == "none" ? "flex" : "none";
-    });
+// Close mobile menu helper func
+const closeMobileMenu = () => {
+	mobileMenuLinks.style.display = 'none'
+}
+
+// Automatically close mobile menu on click outside
+docBody.addEventListener('click', e => {
+	const clickedInside = mobileMenuLinks.contains(e.target)
+	const clickedMobileIcon = mobileMenu.contains(e.target)
+
+	if (mobileMenuLinks.style.display == 'none') {
+		console.log('none')
+		return null
+	}
+	if (
+		mobileMenuLinks.style.display == 'flex' &&
+		!clickedInside &&
+		!clickedMobileIcon
+	) {
+		console.log('click outside')
+		closeMobileMenu()
+	}
+})
+
+// Open and close mobile menu
+mobileMenu.addEventListener('click', () => {
+	mobileMenuLinks.style.display =
+		mobileMenuLinks.style.display == 'none' ? 'flex' : 'none'
+})
 
 // Scroll down home page - offset by nav bar height
 
@@ -23,8 +50,8 @@ mobileMenu.addEventListener("click", () => {
     console.log(window.screenY);
   } */
 
-  // Scroll down home page - offset by nav bar height
-  /* const navbar = document.getElementById('#nav-bar');
+// Scroll down home page - offset by nav bar height
+/* const navbar = document.getElementById('#nav-bar');
   let navbarHeight = navbar.offsetHeight;
   let navbarWidth = navbar.offsetWidth;
   console.log(navbarHeight);
@@ -42,7 +69,7 @@ mobileMenu.addEventListener("click", () => {
   
   console.log(window.scrollY); */
 
- /*  document.addEventListener('DOMContentLoaded', () => {
+/*  document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM content is loaded');
     
   }); */
@@ -51,58 +78,59 @@ mobileMenu.addEventListener("click", () => {
 
 // mobileMenu.style.color = 'red';
 // console.log(mobileMenu.style);
-userHasScrolled = false;
-window.onscroll = function ()
-{
-  //console.log(navbar.style.display);
-  //navbar.style.opacity = 1;
-  if(window.scrollY === 0) {
-    userHasScrolled = false;
-    // navbar.style.opacity = 1;
-
-  } else {
-    userHasScrolled = true;
-    // navbar.style.opacity = 0.5;
-  }
-  console.log(userHasScrolled);
+userHasScrolled = false
+window.onscroll = function () {
+	//console.log(navbar.style.display);
+	//navbar.style.opacity = 1;
+	if (window.scrollY === 0) {
+		userHasScrolled = false
+		// navbar.style.opacity = 1;
+	} else {
+		userHasScrolled = true
+		// navbar.style.opacity = 0.5;
+	}
+	console.log(userHasScrolled)
 }
-console.log(userHasScrolled);
+console.log(userHasScrolled)
 
-
-function scrollHome(){
-  window.scrollTo(0, 0);
+function scrollHome() {
+	window.scrollTo(0, 0)
+	closeMobileMenu()
 }
 
-function scrollWork(){
-    const workBody = document.getElementById('body-work');
-    let headerOffset = 60;
-    let workBodyPosition = workBody.getBoundingClientRect().top;
-    let offsetPosition = workBodyPosition + window.pageYOffset - headerOffset;
+function scrollWork() {
+	const workBody = document.getElementById('body-work')
+	let headerOffset = 60
+	let workBodyPosition = workBody.getBoundingClientRect().top
+	let offsetPosition = workBodyPosition + window.pageYOffset - headerOffset
 
-    window.scrollTo({
-            top: offsetPosition,
-            behavior: "smooth"
-    });
+	window.scrollTo({
+		top: offsetPosition,
+		behavior: 'smooth',
+	})
+	closeMobileMenu()
 }
-function scrollAboutMe(){
-    const aboutMeBody = document.getElementById('body-about-me');
-    let headerOffset = 60;
-    let aboutMeBodyPosition = aboutMeBody.getBoundingClientRect().top;
-    let offsetPosition = aboutMeBodyPosition + window.pageYOffset - headerOffset;
+function scrollAboutMe() {
+	const aboutMeBody = document.getElementById('body-about-me')
+	let headerOffset = 60
+	let aboutMeBodyPosition = aboutMeBody.getBoundingClientRect().top
+	let offsetPosition = aboutMeBodyPosition + window.pageYOffset - headerOffset
 
-    window.scrollTo({
-            top: offsetPosition,
-            behavior: "smooth"
-    });
+	window.scrollTo({
+		top: offsetPosition,
+		behavior: 'smooth',
+	})
+	closeMobileMenu()
 }
-function scrollContact(){
-    const contactBody = document.getElementById('body-contact');
-    let headerOffset = 45;
-    let contactBodyPosition = contactBody.getBoundingClientRect().top;
-    let offsetPosition = contactBodyPosition + window.pageYOffset - headerOffset;
+function scrollContact() {
+	const contactBody = document.getElementById('body-contact')
+	let headerOffset = 45
+	let contactBodyPosition = contactBody.getBoundingClientRect().top
+	let offsetPosition = contactBodyPosition + window.pageYOffset - headerOffset
 
-    window.scrollTo({
-            top: offsetPosition,
-            behavior: "smooth"
-    });
+	window.scrollTo({
+		top: offsetPosition,
+		behavior: 'smooth',
+	})
+	closeMobileMenu()
 }
